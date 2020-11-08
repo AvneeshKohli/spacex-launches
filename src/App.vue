d26674adcc8d9df6e6e36d17a56926b037b5bbbb
AvneeshKohli <avneeshkohli3@gmail.com>
2020-11-09 04:54:43 +0530

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,300 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>SpaceX Launch Programs</h1>
+    <div class="filter-section">
+      <h2>Filters</h2>
+      <p>Launch Year</p>
+      <div class="annual-btns-grid">
+        <button
+          type="button"
+          v-for="(year,index) in years"
+          v-bind:key="index"
+          class="btn"
+          v-on:click="updateView($event,year)"
+        >{{year}}</button>
+      </div>
+      <p>Successful Launch</p>
+      <div class="annual-btns-grid">
+        <button type="button" class="btn" v-on:click="updateView($event,'Launch success')">True</button>
+        <button type="button" class="btn" v-on:click="updateView($event,'Launch fail')">False</button>
+      </div>
+      <p>Successful Landing</p>
+      <div class="annual-btns-grid">
+        <button type="button" class="btn" v-on:click="updateView($event,'Land success')">True</button>
+        <button type="button" class="btn" v-on:click="updateView($event,'Land fail')">False</button>
+      </div>
+    </div>
+    <div v-if="isLodaing" class="loader">
+      <div class="loading-spinner"></div>
+    </div>
+    <div v-if="showCardGrid" class="launch-card-wrapper">
+      <launch-card v-for="launch in launches" :launch="launch" v-bind:key="launch.flight_number" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LaunchCard from "./components/LaunchCard.vue";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    LaunchCard
+  },
+
+  data: () => ({
+    launches: [],
+    showCardGrid: true,
+    years: [],
+    isLodaing: true,
+    basePath: "https://api.spacexdata.com/v3/launches?limit=100",
+    currentPath: ""
+  }),
+  async created() {
+    for (let i = 2006; i <= 2020; i++) {
+      this.years.push(i);
+    }
+    const { data } = await axios.get(
+      "https://api.spacexdata.com/v3/launches?limit=100"
+    );
+    data.forEach(launch => {
+      this.launches.push(launch);
+    });
+    this.isLodaing = false;
+  },
+
+  methods: {
+    updateView(event, val) {
+      if (isNaN(val) === false) {
+        let el = document.querySelector(".active-btn");
+        if (el !== null) {
+          el.classList.remove("active-btn");
+        }
+        let launchYear = "&launch_year=" + val;
+        event.target.classList.add("active-btn");
+        if (this.basePath.includes("launch_year")) {
+          let existingYear = this.basePath.substring(
+            this.basePath.indexOf("&launch_year"),
+            this.basePath.indexOf("&launch_year") + 17
+          );
+
+          this.basePath = this.basePath.replace(existingYear, launchYear);
+        } else {
+          this.basePath = this.basePath + launchYear;
+        }
+      } else if (val.includes("Launch")) {
+        let el = document.querySelector(".active-launch-btn");
+        if (el !== null) {
+          el.classList.remove("active-launch-btn");
+        }
+        event.target.classList.add("active-launch-btn");
+        let new_launch = val.includes("success")
+          ? "&launch_success=true"
+          : "&launch_success=false";
+        if (this.basePath.includes("&launch_success=true")) {
+          this.basePath = this.basePath.replace(
+            "&launch_success=true",
+            new_launch
+          );
+        } else if (this.basePath.includes("&launch_success=false")) {
+          this.basePath = this.basePath.replace(
+            "&launch_success=false",
+            new_launch
+          );
+        } else {
+          this.basePath = this.basePath + new_launch;
+        }
+      } else {
+        //landing block
+        let el = document.querySelector(".active-land-btn");
+        if (el !== null) {
+          el.classList.remove("active-land-btn");
+        }
+        event.target.classList.add("active-land-btn");
+        let new_land = val.includes("success")
+          ? "&land_success=true"
+          : "&land_success=false";
+        if (this.basePath.includes("&land_success=true")) {
+          this.basePath = this.basePath.replace("&land_success=true", new_land);
+        } else if (this.basePath.includes("&land_success=false")) {
+          this.basePath = this.basePath.replace(
+            "&land_success=false",
+            new_land
+          );
+        } else {
+          this.basePath = this.basePath + new_land;
+        }
+      }
+      console.log(this.basePath);
+      this.showCardGrid = false;
+      axios.get(this.basePath).then(response => {
+        this.launches = [];
+        this.launches = response.data;
+      });
+      let url = this.basePath.substring(
+        this.basePath.indexOf("limit") + 10,
+        this.basePath.length
+      );
+      this.$router.push({ path: url, component: this.$el });
+      this.showCardGrid = true;
+    }
   }
-}
+};
+//CODE BY AVNEESH KOHLI
 </script>
 
 <style>
+html {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: #eeeeee;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 100%;
+}
+h2 {
+  margin-top: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  max-width: 1440px;
+  margin: 0 auto;
+  position: relative;
+}
+.loader {
+  background: #eeeeee;
+  opacity: 0.65;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  z-index: 1;
+  height: 100vh;
+}
+.filter-section {
+  background-color: white;
+  display: block;
+  z-index: 1;
+  padding: 1em;
+  border-radius: 8px;
+  margin: 0 0.8em 1em;
+}
+.filter-section p {
+  width: 60%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 20px;
+  margin: 1rem auto;
+  border-bottom: 1px solid #bbb;
+  padding-bottom: 4px;
+}
+.annual-btns-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 0 1em;
+}
+.btn {
+  background-color: #a7ce95;
+  border-radius: 6px;
+  border: none;
+  display: inline-block;
+  cursor: pointer;
+  color: #000000;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 14px;
+  text-decoration: none;
+  width: 41%;
+  margin-bottom: 0.9rem;
+  height: 2.2rem;
+}
+.btn.active-btn {
+  background-color: #6a9c50;
+}
+
+.btn:active {
+  background-color: #6a9c50;
+  position: relative;
+  border: none;
+  top: 1px;
+}
+.btn:focus {
+  outline: none;
+}
+.launch-card-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  margin: 0.8em;
+}
+.cred {
+  position: absolute;
+  bottom: 0;
+}
+@media (min-width: 700px) {
+  .launch-card-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    margin: 0 0 0 28%;
+    position: absolute;
+    top: 3.6rem;
+    min-width: -webkit-fill-available;
+  }
+  .btn {
+    width: 46%;
+    margin-bottom: 0.6rem;
+    height: 1.8rem;
+  }
+  .filter-section p {
+    width: 74%;
+    font-size: 16px;
+  }
+  .btn:hover,
+  .btn.active-btn,
+  .btn.active-launch-btn,
+  .btn.active-land-btn {
+    background-color: #6a9c50;
+  }
+
+  .filter-section {
+    width: 23%;
+    margin: 0;
+  }
+}
+@media (min-width: 1024px) {
+  .launch-card-wrapper {
+    margin-left: 17%;
+  }
+  .btn {
+    width: 46%;
+    margin-bottom: 0.6rem;
+    height: 1.8rem;
+  }
+  .filter-section p {
+    width: 74%;
+    font-size: 16px;
+  }
+  .filter-section {
+    width: 14%;
+  }
+}
+.loading-spinner {
+  width: 100px;
+  height: 100px;
+  border-top: 3px solid #000;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+  position: absolute;
+  left: 45%;
+  transform: translate(-100%, -50%);
+  top: 45%;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
---
+++ src/components/LaunchCard.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="card">
+    <img :src="launch.links.mission_patch_small" class="card-image" alt="my-img" />
+    <p class="launch-name">{{launch.mission_name}} #{{launch.flight_number}}</p>
+    <span class="mission-detail">
+      Mission Ids:
+      <ul>
+        <li class="mission-id-value">{{launch.mission_id[0] | missionIdCheck}}</li>
+      </ul>
+    </span>
+    <div class="info-grid">
+      <div class>Launch Year</div>
+      <div>{{launch.launch_year}}</div>
+      <div>Successful Launch</div>
+      <div>{{launch.launch_success}}</div>
+      <div>Successful Landing</div>
+      <div>{{launch.rocket.first_stage.cores[0].land_success | landSuccessCheck}}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["launch"],
+
+  mounted() {},
+
+  filters: {
+    missionIdCheck(value) {
+      if (typeof value === "undefined") {
+        return "Not Provided";
+      } else {
+        return value;
+      }
+    },
+    landSuccessCheck(value) {
+      if (value === null) {
+        return "No Info";
+      } else {
+        return value;
+      }
+    }
+  }
+};
+//CODE BY AVNEESH KOHLI
+</script>
+<style>
+ul {
+  margin: 0;
+}
+.card {
+  border-radius: 5px;
+  box-shadow: 0 0.1rem 1rem rgba(0, 0, 0, 0.22);
+  padding: 1em;
+  background: #fff;
+  width: 100%;
+  display: table;
+  transition: transform 0.25s;
+  margin: 0 0 1.5em;
+  min-height: 22em;
+}
+.card-image {
+  border-radius: 3px;
+  width: 100%;
+  height: 50%;
+  background-color: #eeeeee;
+}
+.launch-name {
+  color: #223f7a;
+  font-size: 1.2em;
+  font-weight: 600;
+  margin: 0.8rem 0px;
+}
+.mission-detail {
+  display: block;
+  color: #000;
+  font-size: 1.2em;
+  margin: 0 0 0.6rem 0;
+  font-weight: 600;
+}
+.mission-id-value {
+  font-weight: normal;
+  color: #223f7a;
+  font-size: 0.95em;
+}
+.info-grid {
+  display: grid;
+  grid-template-rows: 1.6rem 2rem 2rem;
+  grid-template-columns: 50% 50%;
+  grid-gap: 15px 5px;
+  color: #223f7a;
+  font-size: 1.1em;
+}
+.info-grid div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.info-grid div:nth-child(odd) {
+  color: #000;
+  font-weight: 600;
+}
+@media (min-width: 700px) {
+  .card {
+    padding: 2;
+    width: 41.5%;
+    margin: 0 1% 3.5% 1%;
+  }
+  .launch-name {
+    font-size: 1em;
+    margin: 0.5rem 0px;
+  }
+  .mission-detail {
+    font-size: 1em;
+  }
+  .info-grid {
+    grid-gap: 10px 5px;
+    font-size: 0.9em;
+  }
+}
+@media (min-width: 1024px) {
+  .card {
+    padding: 1% 1% 2%;
+    width: 18.5%;
+    transition: transform 0.25s;
+    margin: 0 1% 3.5% 1%;
+  }
+  .card:hover {
+    transform: scale(1.05);
+  }
+}
+</style>
